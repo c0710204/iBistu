b508d0594225248ed3123fb51d7dc0928735916d
allen <codeday@qq.com>
2012-05-17 13:00:30 +0800

    ready to use localStorage

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,7 +1,38 @@
+/*
+ * This is the main JS.
+ * 
+ * */
 
+$(document).bind("mobileinit", function(){
+    $.mobile.defaultPageTransition = 'fade';
+    $.mobile.allowCrossDomainPages = true;
+    $.support.cors = true;
+    
+});
 
+$("#displayIntroduce").load(
+    "http://m.bistu.edu.cn/api/api.php?table=intro&action=list",
+    function(response,status){
+        $("#displayIntroduce").html = response;
+    }
+);
 
-
+$().ready(function(){
+    //当点击课程按钮时候，课程页面显示。
+    $("#index2course").click(function() {
+        
+        
+        
+        
+        $("#home").hide();
+        $("#course").show();
+        
+        
+    });
+    
+    
+    
+});
 
 
 
